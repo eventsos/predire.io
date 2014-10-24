d2e5ac177b977c83a3d83dabf146871731955693
Simon Opitz <simon@iss-opitz.de>
2014-10-24 22:42:31 +0200

    setup fetch_tweet worker and app credentials

--- workers/fetch_tweets.php
+++ workers/fetch_tweets.php
@@ -6,14 +6,23 @@
  * Time: 22:06
  */
 
-require_once('libs/Pirehose/Phirehose.php');
+require_once('libs/Pirehose/lib/Phirehose.php');
+require_once('libs/Pirehose/lib/OauthPhirehose.php');
 
-class FetchTweetsWorker extends Phirehose {
+define("TWITTER_CONSUMER_KEY", "niBlreyN8veWB4ikPvrwR5lAp");
+define("TWITTER_CONSUMER_SECRET", "ifwuH9DjNHu9Yi3yYQDCludVT7AzbR3PhDDKBPgaQkwHFOCVG2");
+
+
+// The OAuth data for the twitter account
+define("OAUTH_TOKEN", "2842831673-Ss69IHpFJTB3n34PV1irlGRqkQ34ybXuDTa6JPT");
+define("OAUTH_SECRET", "stIK6q4fTDXqZIZAAkatCJ6dtzuaWlCytxLfCWxbKkEjg");
+
+class FetchTweetsWorker extends OauthPhirehose {
     public function enqueueStatus($status) {
         file_put_contents('../data/tweets.txt', $status, FILE_APPEND | LOCK_EX);
     }
 }
 
-$fetchTweetsWorker = new FetchTweetsWorker('filterapi', 'twitterbot', Phirehose::METHOD_FILTER);
+$fetchTweetsWorker = new FetchTweetsWorker(OAUTH_TOKEN, OAUTH_SECRET, Phirehose::METHOD_FILTER);
 $fetchTweetsWorker->setTrack(array('#GOOG', '#SPY', '#GLD', '#DIA', '#QQQ', '#AAPL', '#MSFT', '#PG', '#KFT', '#AA', '$GOOG', '$SPY', '$GLD', '$DIA', '$QQQ', '$AAPL', '$MSFT', '$PG', '$KFT', '$AA'));
 $fetchTweetsWorker->consume();
