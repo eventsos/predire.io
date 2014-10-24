3604a145b57a7d0d2ad873a2b0c8cc1c7057baf4
Simon Opitz <simon@iss-opitz.de>
2014-10-24 22:12:27 +0200

    add basic fetch_tweet worker (+code formatting)

--- workers/fetch_tweets.php
+++ workers/fetch_tweets.php
@@ -4,4 +4,16 @@
  * User: sopitz
  * Date: 24/10/14
  * Time: 22:06
- */ 
+ */
+
+require_once('lib/Pirehose/Phirehose.php');
+
+class FetchTweetsWorker extends Phirehose {
+    public function enqueueStatus($status) {
+        file_put_contents('tweets.txt', $status, FILE_APPEND | LOCK_EX);
+    }
+}
+
+$fetchTweetsWorker = new FetchTweetsWorker('filterapi', 'twitterbot', Phirehose::METHOD_FILTER);
+$fetchTweetsWorker->setTrack(array('#GOOG', '#SPY', '#GLD', '#DIA', '#QQQ', '#AAPL', '#MSFT', '#PG', '#KFT', '#AA', '$GOOG', '$SPY', '$GLD', '$DIA', '$QQQ', '$AAPL', '$MSFT', '$PG', '$KFT', '$AA'));
+$fetchTweetsWorker->consume();
